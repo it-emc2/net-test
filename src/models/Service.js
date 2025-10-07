433fdce093d5dca73ef9b4d91ab7b1533c465110
Joanna Pocztanska <fjn46@interia.eu>
2025-10-07 08:41:17 +0200

    Create Service.js

---
+++ src/models/Service.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const serviceSchema = new mongoose.Schema(
+  {
+    serviceId: { type: String, required: true, unique: true, index: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true, min: 0 },
+
+  },
+  { timestamps: true, collection: 'Services' }
+);
+
+const Service = mongoose.model('Service', serviceSchema);
+export default Service;
