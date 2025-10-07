d8095372e047689c09e317212c782609495ea616
Joanna Pocztanska <fjn46@interia.eu>
2025-10-07 08:46:15 +0200

    services

--- src/models/Service.js
+++ src/models/Service.js
@@ -5,7 +5,7 @@ const serviceSchema = new mongoose.Schema(
     serviceId: { type: String, required: true, unique: true, index: true },
     name: { type: String, required: true },
     price: { type: Number, required: true, min: 0 },
-
+    time: { type: Number, required: true, min: 0 },
   },
   { timestamps: true, collection: 'Services' }
 );
