d25a2640b96d51a2529bbdc7f45605b4462f136e
Joanna Pocztanska <fjn46@interia.eu>
2025-11-17 16:03:25 +0100

    Update Service.js

--- src/models/Service.js
+++ src/models/Service.js
@@ -4,8 +4,11 @@ const serviceSchema = new mongoose.Schema(
   {
     serviceId: { type: String, required: true, unique: true, index: true },
     name: { type: String, required: true },
+    description: { type: String, default: null },
+    internal_name: { type: String, default: null },
     price: { type: Number, required: true, min: 0 },
     time: { type: Number, required: true, min: 0 },
+    source:    { type: String, default: null },
   },
   { timestamps: true, collection: 'Services' }
 );
